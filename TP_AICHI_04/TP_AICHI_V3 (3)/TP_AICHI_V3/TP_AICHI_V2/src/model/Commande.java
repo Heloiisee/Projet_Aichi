889a34399cc9114ef8f0d6c61c75ff5c4f496501
Heloiisee <150189634+Heloiisee@users.noreply.github.com>
2024-12-05 08:34:45 -0400

    Update Commande.java

--- TP_AICHI_04/TP_AICHI_V3 (3)/TP_AICHI_V3/TP_AICHI_V2/src/model/Commande.java
+++ TP_AICHI_04/TP_AICHI_V3 (3)/TP_AICHI_V3/TP_AICHI_V2/src/model/Commande.java
@@ -1,94 +1,76 @@
 package model;
 
+/**
+ * @Author Melissa
+ * Modèle de la classe Commande
+ * @version 1.0
+ */
+
 import java.util.Date;
 
-public class Commande {
-    private int id;
-    private int id_client;
-    private int id_article;
-    private String nom_commande;
-    private Date date;
-    private String statut;
-    private int prix;
+/**
+ * Classe Commande
+ */
 
-    public Commande(int id, int id_client, int id_article, String nom_commande, Date date, String statut, int prix) {
-        this.id = id;
-        this.id_client = id_client;
-        this.id_article = id_article;
-        this.nom_commande = nom_commande;
-        this.date = date;
+public class Commande {
+    private int nCommande;
+    private int idClient;
+    private Date date_Commande;
+    private String statut;
+
+    /**
+     * Constructeur pour la création d'une nouvelle commande avec un nCommande auto-incrémenté.
+     */
+    public Commande(int idClient, Date date_Commande, String statut) {
+        this.idClient = idClient;
+        this.date_Commande = date_Commande;
         this.statut = statut;
-        this.prix = prix;
     }
 
+    /**
+     * Constructeur complet pour l'initialisation de toutes les variables de la commande.
+     */
+    public Commande(int nCommande, int idClient, Date date_Commande, String statut) {
+        this.nCommande = nCommande;
+        this.idClient = idClient;
+        this.date_Commande = date_Commande;
+        this.statut = statut;
+    }
 
-
-    public int getId() {
-        return id;
+    public int getNCommande() {
+        return nCommande;
     }
 
     public int getIdClient() {
-        return id_client;
+        return idClient;
     }
 
-    public int getIdArticle() {
-        return id_article;
-    }
-
-    public String getNomCommande() {
-        return nom_commande;
-    }
-
-    public Date getDateCommande() {
-        return date;
+    public Date getDate_Commande() {
+        return date_Commande;
     }
 
     public String getStatut() {
         return statut;
     }
 
-    public int getPrixTotal() {
-        return prix;
+    public void setNCommande(int nCommande) {
+        this.nCommande = nCommande;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setIdClient(int idClient) {
+        this.idClient = idClient;
     }
 
-    public void setIdClient(int id_client) {
-        this.id_client = id_client;
-    }
-
-    public void setIdArticle(int id_article) {
-        this.id_article = id_article;
-    }
-
-    public void setNomCommande(String nom_commande) {
-        this.nom_commande = nom_commande;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
+    public void setDate_Commande(Date date_Commande) {
+        this.date_Commande = date_Commande;
     }
 
     public void setStatut(String statut) {
         this.statut = statut;
     }
 
-    public void setPrixTotal(int prix) {
-        this.prix = prix;
-    }
-
     @Override
     public String toString() {
-        return "Commande{" +
-                "id=" + id +
-                ", idClient=" + id_client +
-                ", idArticle=" + id_article +
-                ", nomCommande='" + nom_commande + '\'' +
-                ", dateCommande=" + date +
-                ", statut='" + statut + '\'' +
-                ", prixTotal=" + prix +
-                '}';
+        return "Commande{ nCommande=" + nCommande + ", idClient=" + idClient + ", date_Commande=" + date_Commande + ", statut=" + statut + '}';
     }
 }
